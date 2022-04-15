11384e61b833ee89ac4e2d7a161722698fbb0e4f
Gulnihal Naldoken <gh.naldoken@gmail.com>
2022-04-15 21:20:05 +0100

    feat: add Header

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import TopNav from "./TopNav";
+import Navbar from "./Navbar";
+
+export default function Header() {
+	return (
+		<>
+			<div>
+				<TopNav />
+				<Navbar />
+			</div>
+		</>
+	);
+}
