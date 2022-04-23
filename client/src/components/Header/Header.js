5af1e31bbda700367da87ef04be75245d04b802a
Gulnihal Naldoken <gh.naldoken@gmail.com>
2022-04-23 14:23:37 +0100

    feat: add header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import TopNav from "./TopNav";
 import Navbar from "./Navbar";
+import Categories from "../Main/Categories";
+import "./Header.css";
 
 export default function Header() {
 	return (
 		<>
-			<div>
+			<div className="header">
 				<TopNav />
 				<Navbar />
+				<Categories />
 			</div>
 		</>
 	);
