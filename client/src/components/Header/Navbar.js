520070acd22bd9432f0e2c7386a6c69e1e2ecb10
Gulnihal Naldoken <gh.naldoken@gmail.com>
2022-04-23 14:24:54 +0100

    feat: add navbar

--- client/src/components/Header/Navbar.js
+++ client/src/components/Header/Navbar.js
@@ -2,26 +2,62 @@ import React, { useState } from "react";
 import { ImSearch } from "react-icons/im"
 import { FiSmile, FiShoppingBag } from "react-icons/fi";
 import { BiWorld } from "react-icons/bi"
+import { Link } from "react-router-dom";
 import "../Header/Header.css";
+import {
+	MdOutlineMenu,
+	MdClose,
+} from "react-icons/md";
 
 export default function Navbar () {
-    const [isSearchOpen, setIsSearchOpen] = useState(false);
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
 			<>
 				<section className="navbar-container">
-					<h1 className="logo">SPLUSH</h1>
-					<div className="search">
-						<input
-							className={`${isSearchOpen ? "open" : ""}`}
-							type="text"
-							placeholder="Search..."
-						/>
-						<ImSearch className="navbar-icon" />
+					<div>
+						<h1 className="logo">SPLUSH</h1>
+					</div>
+					<div className="logo-hamburger-container">
+						<button
+							aria-label="hamburger menu"
+							className="hamburger"
+							onClick={() => setIsMenuOpen((prev) => !prev)}
+						>
+							{!isMenuOpen ? <MdOutlineMenu /> : <MdClose />}
+						</button>
+						<nav className={`${isMenuOpen ? "open" : ""}`}>
+							<ul onClick={() => setIsMenuOpen(false)}>
+								<li>
+									Account
+								</li>
+								<li>
+									Region
+								</li>
+								<li>
+									<Link to="/cart">
+										<FiShoppingBag className="navbar-icon" />
+									</Link>
+								</li>
+								<li>
+									<input type="text" placeholder="Search..." />
+									<ImSearch className="navbar-icon" />
+								</li>
+							</ul>
+						</nav>
 					</div>
 					<article className="navbar-menu">
+						<div className="search">
+							<input
+								type="text"
+								placeholder="Search..."
+								/>
+							<ImSearch className="navbar-icon" />
+						</div>
 						<FiSmile className="navbar-icon" />
 						<BiWorld className="navbar-icon" />
-						<FiShoppingBag className="navbar-icon" />
+						<Link to="/cart">
+							<FiShoppingBag className="navbar-icon" />
+						</Link>
 					</article>
 				</section>
 			</>
