2b514f13ae9ed827d92abf23bab03c336c070965
Gulnihal Naldoken <gh.naldoken@gmail.com>
2022-04-25 09:57:21 +0100

    feat: add localstorage and check out functionality

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,32 +1,66 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import CartItem from "../components/Main/CartItem";
+// import Loading from "../components/Main/Loading";
 import "../pages/styles/Cart.css";
 
 
 export default function Cart() {
-	const [allInCart, setAllInCart] = useState([]);
+	const [currentCart, setCurrentCart] = useState([]);
+	const [isAmountChange, setIsAmountChange] = useState(false);
+	const [isSubmitting, setIsSubmitting] = useState(false);
 
-	// const allCart = allInCart.map((item) => (
-	// 	<CartItem
-	// 		key={product.productid}
-	// 		product={product}
-	// 	/>
-	// ));
+	useEffect(() => {
+		setCurrentCart(JSON.parse(localStorage.getItem("cartStorage") || "[]"));
+		console.log(currentCart);
+	}, [isAmountChange]);
+
+	const total = currentCart.reduce((acc, curr) => {
+		return acc += curr.count * curr.pricing.priceRange.start.gross.amount;
+	}, 0)
+
+	const totalItems = currentCart.reduce((acc, curr) => {
+		return (acc += curr.count);
+	}, 0);
+	
+	function HandleCheckout () {
+		setIsSubmitting(true);
+		setCurrentCart([]);
+		localStorage.setItem("cartStorage", JSON.stringify([]));
+		// navigate Checkout done!
+	}
+	
+	function RemoveItemHandle (id) {
+		const removedItem = currentCart.filter((item) => item.id != id);
+		setCurrentCart(removedItem);
+	}
 	return (
 		<>
+			{/* <Loading isLoading={isSubmitting} /> */}
 			<section className="cart-page">
 				<article className="cart-header">
 					<h2>Your Bag</h2>
 					<p>Items in your bag are not reserved.</p>
 				</article>
-				<article className="cart-items-container">{allInCart}</article>
+				<article className="cart-items-container">
+					{currentCart.map((item) => {
+						return (
+							<CartItem
+								key={item.id}
+								item={item}
+								totalItems={totalItems}
+								total={total}
+								setIsAmountChange={setIsAmountChange}
+								RemoveItemHandle={RemoveItemHandle}
+							/>
+						);
+					})}
+				</article>
 				<article className="cart-summary">
 					<p className="summary">Order Summary</p>
 					<div className="amount">
-						<p>{`${allInCart.length} item${
-							allInCart.length > 1 ? "s" : ""
-						}`}</p>
-						<p>£ 00.00</p>
+						<p>{`${totalItems} item${totalItems > 1 ? "s" : ""}`}</p>
+						<p>£ {total.toFixed(2)}</p>
 					</div>
 					<div className="cart-summary-delivery">
 						<p>Delivery</p>
@@ -36,7 +70,9 @@ export default function Cart() {
 						<p>TOTAL</p>
 					</div>
 				</article>
-				<button className="checkout-btn">CHECKOUT</button>
+				<button className="checkout-btn" onClick={() => HandleCheckout()}>
+					CHECKOUT
+				</button>
 				<Link to="/">
 					<button className="checkout-btn continue">
 						Continue to Shopping
